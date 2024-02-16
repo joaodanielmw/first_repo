b49e1d4018297ad642e30ac89d323ec7a6a4c242
unknown <joaomweng@gmail.com>
2024-02-15 23:32:28 -0300

    Hello World

---
+++ test.lua
@@ -0,0 +1,28 @@
+-->VARIABLES<--
+
+--Private
+local modem = peripheral.find("modem") or error("Connection failed: Modem not found")
+
+--Public
+
+-->METHODS<--
+
+--Private
+local function sortByType(type)
+    local names = modem.getNamesRemote()
+    local sorted = {}
+
+    for i = 1, #names do
+        local n = names[i]
+        if peripheral.getType(n) == type then
+            sorted[n] = n
+        end
+    end
+
+    return sorted
+end
+
+--Public
+
+-->RETURN<--
+return {}
